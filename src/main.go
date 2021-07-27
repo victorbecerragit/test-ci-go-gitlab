4bc681e42497bf0c2ed8eb2932a96b809d4385dd
victorbecerragit <vbecerraar@gmail.com>
2021-07-27 17:00:42 +0200

    add go files and dockerfile

---
+++ src/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-redis/redis"
+	"github.com/gofiber/fiber/v2"
+)
+
+func greet(n int64) string {
+	return fmt.Sprintf("Hello, visitor number %d!", n)
+}
+
+func main() {
+
+	redisdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_URL"),
+	})
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		result, err := redisdb.Incr("counter").Result()
+		if err != nil {
+			panic(err)
+		}
+		return c.SendString(greet(result))
+	})
+
+	log.Fatal(app.Listen(os.Getenv("SERVICE_ADDR")))
+
+}
